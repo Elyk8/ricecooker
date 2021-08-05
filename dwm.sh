225f3a269e1ae13342f39cd1aba820f9250f07a9
Kyle Se <kyle@skynet.localdomain>
2021-08-05 13:45:29 +1000

    commit

--- dwm.sh
+++ dwm.sh
@@ -3,27 +3,27 @@
 sudo timedatectl set-ntp true
 sudo hwclock --systohc
 sudo reflector -c Australia -c "New Zealand" -a 12 -p https -p http --sort rate --save /etc/pacman.d/mirrorlist
-sudo pacman -Syu --noconfirm
+sudo pacman -Sy
 
-sudo pacman -S --noconfirm acpilight xorg sx simplescreenrecorder ttf-bitstream-vera ttf-dejavu ttf-liberation ttf-devicons ttf-linux-libertine noto-fonts tex-gyre-fonts ttf-jetbrains-mono adobe-source-code-pro-fonts noto-fonts-cjk noto-fonts-emoji
-sudo pacman -S --noconfirm zathura zathura-pdf-mupdf calcurse dash dunst exa feh flameshot inkscape lxappearance-gtk3 lxsession-gtk3 mpc mpd mpv ncmpcpp wmctrl lazygit newsboat redshift rofi rofi-pass trash-cli interception-dual-function-keys
+sudo pacman -S --noconfirm acpilight xorg sx simplescreenrecorder ttf-bitstream-vera ttf-dejavu ttf-liberation ttf-devicons ttf-linux-libertine noto-fonts tex-gyre-fonts ttf-jetbrains-mono adobe-source-code-pro-fonts noto-fonts-cjk noto-fonts-emoji zathura zathura-pdf-mupdf calcurse dash dunst exa feh flameshot inkscape lxappearance-gtk3 lxsession-gtk3 mpc mpd mpv ncmpcpp wmctrl lazygit newsboat redshift rofi rofi-pass trash-cli interception-dual-function-keys
 
-sudo systemctl enable --now udevmon.service
+#sudo systemctl enable --now udevmon.service
 
 # AUR packages
 git clone https://aur.archlinux.org/paru.git
 cd paru
 makepkg -si
-paru -S --noconfirm beautyline brave-bin dragon-drag-and-drop gotop-bin lf mpd-notification-git mutt-wizard nerd-fonts-jetbrains-mono nerd-fonts-monoki pfetch zramd
+paru -S --noconfirm beautyline brave-bin dragon-drag-and-drop gotop-bin lf mpd-notification-git mutt-wizard nerd-fonts-jetbrains-mono nerd-fonts-monoki pfetch zramd ttf-twemoji libxft-bgra-git
 
 git config --global credential.helper cache
 
 # Deploy my dotfiles
+cd
 sudo rm -r $HOME/.config $HOME/.local/bin $HOME/.local/share/applications
 mkdir -p $HOME/.local/share/dotrice
 git clone --bare https://github.com/Elyk8/dotrice.git $HOME/.local/share/dotrice
 alias rice='/usr/bin/git --git-dir=$HOME/.local/share/dotrice --work-tree=$HOME'
-rice --local status.showUntrackedFiles no
+rice config --local status.showUntrackedFiles no
 rice checkout
 
 # Install suckless tools
@@ -47,7 +47,8 @@ git clone https://github.com/Elyk8/lvim.git
 cd
 
 # Change to dash
-sudo ln -sfT dash /usr/bin/sh
+cd $HOME/ricecooker
+sudo ln -sfT /usr/bin/dash /usr/bin/sh
 sudo cp ./pachooks/bash-update.hook /usr/share/libalpm/hooks
 
 /bin/echo -e "\e[1;32mREBOOTING IN 5..4..3..2..1..\e[0m"
--- install-uefi.sh
+++ install-uefi.sh
@@ -15,11 +15,11 @@ echo root:password | chpasswd
 # You can add xorg to the installation packages, I usually add it at the DE or WM install script
 # You can remove the tlp package if you are installing on a desktop or vm
 
-pacman -S grub efibootmgr networkmanager network-manager-applet dialog wpa_supplicant mtools dosfstools reflector base-devel linux-headers avahi xdg-user-dirs xdg-utils gvfs gvfs-smb dnsutils bluez bluez-utils cups alsa-utils pipewire pipewire-alsa pipewire-pulse bash-completion reflector acpi acpi_call tlp ipset firewalld acpid os-prober ntfs-3g terminus-font
+pacman -S --noconfirm grub efibootmgr networkmanager network-manager-applet dialog wpa_supplicant mtools dosfstools reflector base-devel linux-headers avahi xdg-user-dirs xdg-utils gvfs gvfs-smb dnsutils bluez bluez-utils cups alsa-utils pipewire pipewire-alsa pipewire-pulse bash-completion reflector acpi acpi_call tlp ipset firewalld acpid os-prober ntfs-3g terminus-font
 
-# pacman -S --noconfirm xf86-video-amdgpu
-# pacman -S --noconfirm nvidia nvidia-utils nvidia-settings
-pacman -S --noconfirm nvidia nvidia-utils nvidia-settings xf86-video-intel
+#pacman -S --noconfirm xf86-video-amdgpu
+#pacman -S --noconfirm nvidia nvidia-utils nvidia-settings
+#pacman -S --noconfirm nvidia nvidia-utils nvidia-settings xf86-video-intel
 
 grub-install --target=x86_64-efi --efi-directory=/boot/efi --bootloader-id=GRUB
 grub-mkconfig -o /boot/grub/grub.cfg
@@ -34,7 +34,8 @@ systemctl enable acpid
 
 useradd -m elyk
 echo elyk:password | chpasswd
-usermod -aG libvirt elyk
+#usermod -aG libvirt elyk
+usermod -aG video,audio elyk
 
 echo "elyk ALL=(ALL) ALL" >> /etc/sudoers.d/elyk
 
